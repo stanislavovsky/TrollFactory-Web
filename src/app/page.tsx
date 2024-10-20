fb4c9af1c04072bb47d83d43d783f8d140d3039d
isinia <181978133+isinia@users.noreply.github.com>
2024-10-20 20:41:43 +0200

    fix: class->className in tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
                 <input type="text" className="grow" placeholder="Last name" />
               </label>
             </div>
-            <button class="btn" id="generate-btn">
+            <button className="btn" id="generate-btn">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
               </svg>

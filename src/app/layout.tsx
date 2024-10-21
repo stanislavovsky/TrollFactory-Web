b8d1c37502dc9cfaaf981626a3c9bc907a4f2566
isinia <181978133+isinia@users.noreply.github.com>
2024-10-21 16:45:04 +0200

    feat: add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="TrollFactory" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>
         {children}
       </body>
